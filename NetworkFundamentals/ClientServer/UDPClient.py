c38194a3b83ddad31c2e65b5e33ba97de8b6191f
Matthew Ashley <75866043+Z-710@users.noreply.github.com>
2024-06-24 14:04:47 +1000

    Add files via upload

---
+++ NetworkFundamentals/ClientServer/UDPClient.py
@@ -0,0 +1,9 @@
+from socket import *
+serverName = '172.19.134.122'
+serverPort = 12000
+clientSocket = socket(AF_INET, SOCK_DGRAM)
+message = input('Input lowercase sentence:')
+clientSocket.sendto(message.encode(),(serverName, serverPort))
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+print(modifiedMessage.decode())
+clientSocket.close()
